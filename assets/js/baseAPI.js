f5b8f5604d6adfe0f018c94342ba8837cb54a3d1
aoziao <634927652@qq.com>
2020-09-02 10:52:11 +0800

    完成了主页开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,20 @@
 $.ajaxPrefilter(function (options) {
   options.url = "http://ajax.frontend.itheima.net" + options.url;
+
+  if (options.url.indexOf("/my/") !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem("token") || " ",
+    };
+  }
+
+  //身份验证
+  options.complete = function (res) {
+    if (
+      res.responseJSON.status === 1 &&
+      res.responseJSON.message === "身份认证失败！"
+    ) {
+      localStorage.removeItem("token");
+      location.href = "../../login.html";
+    }
+  };
 });
---
+++ assets/js/index.js
@@ -0,0 +1,56 @@
+$(function () {
+  getUserInfo();
+
+  //点击退出
+  $("#btnLogOut").on("click", function (e) {
+    e.preventDefault();
+
+    layer.open({
+      icon: 3,
+      title: "提示",
+      content: "确认退出登录?",
+      btn: ["确定", "取消"],
+      yes: function () {
+        localStorage.removeItem("token");
+        location.href = "../../login.html";
+      },
+      cancel: function () {
+        //右上角关闭回调
+        return false; //开启该代码可禁止点击该按钮关闭
+      },
+    });
+  });
+
+  //获取个人信息
+  function getUserInfo() {
+    $.ajax({
+      type: "GET",
+      url: "/my/userinfo",
+      success: function (res) {
+        if (res.status !== 0) {
+          //   console.log(res.message);
+          return layer.msg(res.message);
+        } else {
+          //   console.log(res.data);
+          renderUserInfo(res.data);
+        }
+      },
+
+    });
+  }
+
+  //渲染个人信息
+  function renderUserInfo(data) {
+    let name = data.nickname || data.username;
+    $("#welcome").html("欢迎&nbsp;&nbsp;" + name);
+
+    if (data.user_pic !== null) {
+      $(".layui-nav-img").attr("src", data.user_pic).show();
+      $(".text-avatar").hide();
+    } else {
+      htmlStr = name[0].toUpperCase();
+      $(".text-avatar").html(htmlStr).show();
+      $(".layui-nav-img").hide();
+    }
+  }
+});
